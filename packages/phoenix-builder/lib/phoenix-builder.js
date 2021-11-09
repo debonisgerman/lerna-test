28db2b7041a61cb7094b6c04a2e98e7767f13ccf
Germán <debonis.german@gmail.com>
2021-11-09 10:45:41 -0500

    publish change

--- packages/phoenix-builder/lib/phoenix-builder.js
+++ packages/phoenix-builder/lib/phoenix-builder.js
@@ -5,6 +5,7 @@ const resolve = require('@rollup/plugin-node-resolve').default;
 const babel = require('@rollup/plugin-babel').default;
 const postcss = require('rollup-plugin-postcss');
 
+
 const currentWorkingPath = process.cwd();
 // Little refactor from where we get the code
 const { src, name } = require(path.join(currentWorkingPath, 'package.json'));
